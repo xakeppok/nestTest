26dec964a959d7175be24ce3161f129c53a555d7
Vladislav Kritskiy <vlad@Vladislavs-iMac.local>
2019-07-31 18:51:40 +0300

    testing nestJS

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,19 +1,22 @@
 import { CarsService } from './cars.service';
 import { Response } from 'express';
 import { Controller, Get, Post, Body, Res, HttpStatus } from '@nestjs/common';
+import { ICars } from './interfaces/car.interface';
 
 @Controller('cars')
 export class CarsController {
   constructor(private readonly cars: CarsService) {}
   @Get('/')
-  async findAll(): Promise<any> {
-
-    console.log("xuy")
-    return this.cars.getAll();
+  async findAll(@Res() res: Response): Promise<any> {
+    const cars = await this.cars.getAll();
+    return res.status(HttpStatus.OK).json({ data: cars });
   }
   @Post('create')
-  async create(@Body() createCatDto: any,  @Res() res: Response): Promise<Response> {
-    const car = await this.cars.create(createCatDto);
-    return res.status(HttpStatus.OK).json({data: car});
+  async create(
+    @Body() createCatDto: any,
+    @Res() res: Response,
+  ): Promise<Response> {
+    const car: ICars = await this.cars.create(createCatDto);
+    return res.status(HttpStatus.OK).json({ data: car });
   }
 }
